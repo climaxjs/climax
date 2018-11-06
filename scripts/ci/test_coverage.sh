a613dfe95e22404871af0c7d679a796ecb565e0c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-06 13:47:21 +0100

    scripts/ci/test_coverage: create

---
+++ scripts/ci/test_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://www.gnu.org/software/bash/manual/bashref.html#The-Set-Builtin
+set -e
+
+yarn run test:unit - --coverage
+./node_modules/.bin/coveralls < ./coverage/lcov.info
