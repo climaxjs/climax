d672c47ba4e28288e510d673aae346d0ac46769d
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-19 17:09:59 +0100

    jest.config: switch to Typescript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
+// https://basarat.gitbooks.io/typescript/docs/testing/jest.html
+
 module.exports = {
   bail: true,
-  roots: ['lib/'],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  roots: ['<rootDir>/lib'],
+  testRegex: `(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$`,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   verbose: true,
 }
