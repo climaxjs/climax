2df3e721b9041f83db05d765648bf4e268118749
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-20 11:07:59 +0100

    lib/utils/validateOptionSlug: allow longhand slug alone & "--foo=bar"

--- lib/utils/validateOptionSlug.ts
+++ lib/utils/validateOptionSlug.ts
@@ -4,11 +4,8 @@
  * @example
  * "-f, --foo"
  * "-F, --foo-bar"
- *
- * TODO Allow longhand <slug> alone.
- * TODO Allow "-f, --foo=bar".
  */
 
 export default function validateOptionSlug(slug: string): boolean {
-  return /^-[a-zA-Z]\s--([a-z]((-[a-z]+)+)|[a-z]{2,}((-[a-z]+)+)?)$/.test(slug)
+  return /^(-[a-zA-Z]\s)?--([a-z]((-[a-z]+)+)|[a-z]{2,}((-[a-z]+)+)?)(=[\a-zA-Z]+)?$/.test(slug)
 }
