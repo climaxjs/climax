475251f9d9d8f9facf88a06acb8ce0ebb6a46df4
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-20 10:22:40 +0100

    lib/program/Option: add todo

--- lib/program/Option.ts
+++ lib/program/Option.ts
@@ -1,6 +1,5 @@
 /**
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * TODO Validate <slug> uniqueness.
  */
 
 import errors from '../errors'
