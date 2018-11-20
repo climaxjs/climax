ad5e067ea557dfde88e1ac48b87758bbcddd5911
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-20 11:08:07 +0100

    lib/utils/validateOptionSlug.spec: update

--- lib/utils/validateOptionSlug.spec.ts
+++ lib/utils/validateOptionSlug.spec.ts
@@ -4,8 +4,8 @@ const INVALID_SLUGS = [
   '',
   '-',
   '--',
+  ' -f',
   '-f',
-  '--foo',
   '-f --foo-',
   '-fo --foo',
   '-f --foo1',
@@ -14,13 +14,18 @@ const INVALID_SLUGS = [
   '-f  --foo-bar',
   '-f --foo-bar-',
   '-f --foo-bar-lambda-',
+  ' --foo',
+  '--foo=',
 ]
 const VALID_SLUGS = [
+  '--foo',
+  '--foo=bar',
   '-f --foo',
   '-F --foo-bar',
   '-f --foo-bar-lambda',
   '-f --f-b',
   '-f --f-b-l',
+  '-f --foo=bar',
 ]
 
 INVALID_SLUGS.map(slug => test(`"${slug}" is invalid`, () => expect(validateOptionSlug(slug)).toBe(false)))
