2af8179ecc0b6e2a6792e743ef6e80f1c74a5f37
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-25 23:09:35 +0100

    lib/program/Option.spec: update

--- lib/program/Option.spec.ts
+++ lib/program/Option.spec.ts
@@ -12,11 +12,11 @@ describe(`Option`, () => {
     it(`should fail with a malformed <slug>`, () =>
       expect(() => new Option('foo', _)).toThrow(errors.dictionary.ERR_OPTION_SLUG_VALIDATION_FORMAT))
     it(`should fail with a wrong typed <description>`, () =>
-      expect(() => new Option('-f --foo', 1337 as any)).toThrow(errors.dictionary.ERR_OPTION_DESCRIPTION_VALIDATION_TYPE))
+      expect(() => new Option('-f, --foo', 1337 as any)).toThrow(errors.dictionary.ERR_OPTION_DESCRIPTION_VALIDATION_TYPE))
     it(`should fail with an empty string <description>`, () =>
-      expect(() => new Option('-f --foo', '')).toThrow(errors.dictionary.ERR_OPTION_DESCRIPTION_VALIDATION_LENGTH))
+      expect(() => new Option('-f, --foo', '')).toThrow(errors.dictionary.ERR_OPTION_DESCRIPTION_VALIDATION_LENGTH))
     it(`should fail with a wrong typed <filter>`, () =>
-      expect(() => new Option('-f --foo', 'foo', 1337 as any)).toThrow(errors.dictionary.ERR_OPTION_FILTER_VALIDATION_TYPE))
+      expect(() => new Option('-f, --foo', 'foo', 1337 as any)).toThrow(errors.dictionary.ERR_OPTION_FILTER_VALIDATION_TYPE))
 
     // it(`should pass with a valid description`, () => expect(() => program.description('test')).not.toThrow())
     // it(`should have set the valid description`, () => expect(program.description()).toStrictEqual('test'))
