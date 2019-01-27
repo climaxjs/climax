dec831afe0064f629203c5096f263b2c159f5afc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 00:06:00 +0100

    lib/program/Option: split internal and custom filter validations

--- lib/program/Option.ts
+++ lib/program/Option.ts
@@ -4,18 +4,21 @@
 
 import errors from '../errors'
 import validateOptionSlug from '../utils/validateOptionSlug'
+
 /// <reference path="./type.ts" />
 
 export default class Option {
   public description: string
   public slug: string
-  public filter?: Program.OptionFilter<Program.OptionFilterOutput>
+  public filter?: Filter.Is | Program.OptionFilter<Program.OptionFilterOutput>
 
   constructor(
     slug: string,
     description: string,
-    filter?: Program.OptionFilter<Program.OptionFilterOutput>,
+    filter?: Filter.Is | Program.OptionFilter<Program.OptionFilterOutput>,
   ) {
+    console.log(typeof filter)
+    if (filter !== undefined && typeof filter === 'object') console.log(filter.constructor.name)
     switch (true) {
       case typeof slug !== 'string':
         throw errors.error.ERR_OPTION_SLUG_VALIDATION_TYPE
@@ -32,12 +35,30 @@ export default class Option {
       case description.length === 0:
         throw errors.error.ERR_OPTION_DESCRIPTION_VALIDATION_LENGTH
 
-      case typeof filter !== 'undefined' && typeof filter !== 'function':
-        throw errors.error.ERR_OPTION_FILTER_VALIDATION_TYPE
+      case filter !== undefined:
+        switch (true) {
+          case this.isUnusableInternalFilter(filter) :
+            throw errors.error.ERR_OPTION_FILTER_INTERNAL_VALIDATION_TYPE
+
+          case !this.isInternalFilter(filter) && typeof filter !== 'function':
+            throw errors.error.ERR_OPTION_FILTER_CUSTOM_VALIDATION_TYPE
+        }
     }
 
     this.description = description
     this.slug = slug
     this.filter = filter
+  }
+
+  private isInternalFilter(filter: any): boolean {
+    return typeof filter === 'object' && (
+      ['IsBoolean', 'IsNumber', 'IsString'].includes(filter.constructor.name)
+    )
+  }
+
+  private isUnusableInternalFilter(filter: any): boolean {
+    return typeof filter === 'object' && (
+      ['Is', 'IsObligation', 'IsType'].includes(filter.constructor.name)
+    )
   }
 }
