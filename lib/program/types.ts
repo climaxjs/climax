203ad98e78c92f77a66fc9fcf82a004befcbdd4a
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 00:14:43 +0100

    lib/program/types: simplify

--- lib/program/types.ts
+++ lib/program/types.ts
@@ -1,9 +1,7 @@
+/// <reference path="../filter-factory/type.ts">
+
 namespace Program {
-  export type OptionFilter<T extends OptionFilterOutput> = (data: OptionFilterData) => T
-  export type OptionFilterData = {
-    command: string,
-    slug: string,
-    value?: string,
-  }
+  export type OptionFilter<T extends OptionFilterOutput> = Filter.IsUsable | OptionFilterCustom<T>
+  export type OptionFilterCustom<T extends OptionFilterOutput> = (value?: string) => T
   export type OptionFilterOutput = boolean | number | string
 }
