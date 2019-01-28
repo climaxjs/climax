bdea16a9afec00e89eb4306acc931e26aa7b35a6
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 11:29:19 +0100

    lib/utils: create index

---
+++ lib/utils/index.ts
@@ -0,0 +1,11 @@
+import getErrorWith from './getErrorWith'
+import parseArgs from './parseArgs'
+import validateOptionSlug from './validateOptionSlug'
+import validateSemVer from './validateSemVer'
+
+export {
+  getErrorWith,
+  parseArgs,
+  validateOptionSlug,
+  validateSemVer,
+}
