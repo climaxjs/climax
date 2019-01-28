610009c5e8767ccb4802de729ef7e23a26794098
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 11:23:11 +0100

    lib/program: split Command inheritance

--- lib/program/index.ts
+++ lib/program/index.ts
@@ -1,20 +1,56 @@
 import * as R from 'ramda'
 
 import Command from '../command'
+import Option from '../option'
 import errors from '../errors'
 import validateSemVer from '../utils/validateSemVer'
 
 import * as T from './types'
 import * as CommandT from '../command/types'
+import * as OptionT from '../option/types'
 
-class Program extends Command implements T.Program {
+class Program implements T.Program {
   private _commands: T.ProgramCommands = {}
+  private _description: string
   private _name: string
+  private _options: OptionT.Option[] = []
   private _version: string
 
-/**
- * Customize the program name, replacing the package.json name.
- */
+  /**
+   * Get/Set the program description.
+   *
+   * @description
+   * Unless explicitly set, the package.json description is the default value.
+   */
+  public description(description?: string): string | Program {
+    if (typeof description === 'undefined') {
+      switch (true) {
+        case this._description === undefined:
+          throw errors.error.ERR_COMMAND_DESCRIPTION_UNDEFINED
+      }
+
+      return this._description
+    }
+
+    switch (true) {
+      case typeof description !== 'string':
+        throw errors.error.ERR_COMMAND_DESCRIPTION_VALIDATION_TYPE
+
+      case description.length === 0:
+        throw errors.error.ERR_COMMAND_DESCRIPTION_VALIDATION_LENGTH
+    }
+
+    this._description = description
+
+    return this
+  }
+
+  /**
+   * Get/Set the program name.
+   *
+   * @description
+   * Unless explicitly set, the package.json name is the default value.
+   */
   public name(name?: string): string | Program {
     if (typeof name === 'undefined') {
       switch (true) {
@@ -39,7 +75,10 @@ class Program extends Command implements T.Program {
   }
 
   /**
-   * Customize the program version, replacing the package.json description.
+   * Get/Set the program version.
+   *
+   * @description
+   * Unless explicitly set, the package.json version is the default value.
    */
   public version(version?: string): string | Program {
     if (typeof version === 'undefined') {
@@ -68,6 +107,19 @@ class Program extends Command implements T.Program {
   }
 
   /**
+   * Add a new program option.
+   */
+  public option(
+    slug: string,
+    description: string,
+    filter?: OptionT.OptionFilter,
+  ): Program {
+    this._options.push(new Option(slug, description, filter))
+
+    return this
+  }
+
+  /**
    * Add a new command.
    */
   public command(slug: string): CommandT.Command {
@@ -88,16 +140,13 @@ class Program extends Command implements T.Program {
    * Initiate the program once all the commands and options has been set.
    */
   public run(): void {
-    this.validateCommands()
+    this.validateCommandsProps()
   }
 
   /**
-   * Validate program and commands required properties.
+   * Validate the commands required properties.
    */
-  private validateCommands(): void {
-    this.description()
-    this.name()
-
+  private validateCommandsProps(): void {
     for (const command in this._commands) {
       this._commands[command].description()
     }
