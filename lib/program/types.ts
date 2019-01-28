fbdc54250a6ac27f4433da0648a3549606d0a850
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 11:23:54 +0100

    lib/program/types: remove Command inheritance

--- lib/program/types.ts
+++ lib/program/types.ts
@@ -1,11 +1,14 @@
 import * as Command from '../command/types'
+import * as Option from '../option/types'
 
 export type ProgramCommands = {
   [slug: string]: Command.Command
 }
 
-export interface Program extends Command.Command {
+export interface Program {
   command(slug: string): Command.Command
+  description(slug: string): string | Program
   name(name?: string): string | Program
+  option(slug: string, description: string, filter?: Option.OptionFilter): Program
   version(version?: string): string | Program
 }
