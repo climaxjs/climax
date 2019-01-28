f0ddb86d2bae5aa388339feb0da21807f5658c13
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 11:30:26 +0100

    lib/command/types: lint

--- lib/command/types.ts
+++ lib/command/types.ts
@@ -3,5 +3,6 @@ import * as Program from '../program/types'
 
 export interface Command {
   description(description?: string): string | Command | Program.Program
+
   option(slug: string, description: string, filter?: Option.OptionFilter): Command | Program.Program
 }
