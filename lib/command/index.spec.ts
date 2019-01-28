0e021832cf048d2b60c8ae8b8caf661d7476e83b
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 12:50:24 +0100

    lib/command.spec: fix tests

--- lib/command/index.spec.ts
+++ lib/command/index.spec.ts
@@ -1,17 +1,17 @@
 import Command from '.'
 import errors from '../errors'
 
-describe(`Program`, () => {
+describe(`Command`, () => {
   describe(`#description`, () => {
     const command = new Command()
 
     it(`should fail to be gotten with an unset description`, () =>
-      expect(() => command.description()).toThrow(errors.dictionary.ERR_PROGRAM_DESCRIPTION_UNDEFINED))
+      expect(() => command.description()).toThrow(errors.dictionary.ERR_COMMAND_DESCRIPTION_UNDEFINED))
 
     it(`should fail with a wrong type`, () =>
-      expect(() => command.description(123 as any)).toThrow(errors.dictionary.ERR_PROGRAM_DESCRIPTION_VALIDATION_TYPE))
+      expect(() => command.description(123 as any)).toThrow(errors.dictionary.ERR_COMMAND_DESCRIPTION_VALIDATION_TYPE))
     it(`should fail with an empty string`, () =>
-      expect(() => command.description('')).toThrow(errors.dictionary.ERR_PROGRAM_DESCRIPTION_VALIDATION_LENGTH))
+      expect(() => command.description('')).toThrow(errors.dictionary.ERR_COMMAND_DESCRIPTION_VALIDATION_LENGTH))
 
     it(`should pass with a valid description`, () => expect(() => command.description('test')).not.toThrow())
     it(`should have set the valid description`, () => expect(command.description()).toStrictEqual('test'))
