47f71861b73495ad56d40ced30eaad21cdbcf9ca
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-28 10:55:13 +0100

    lib/command/types: add missing return types

--- lib/command/types.ts
+++ lib/command/types.ts
@@ -1,6 +1,7 @@
 import * as Option from '../option/types'
+import * as Program from '../program/types'
 
 export interface Command {
-  description(description?: string): string | Command
-  option(slug: string, description: string, filter?: Option.OptionFilter): Command
+  description(description?: string): string | Command | Program.Program
+  option(slug: string, description: string, filter?: Option.OptionFilter): Command | Program.Program
 }
