26f5b35f0c2299daf22c081509197e45094788e3
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-29 12:15:34 +0100

    lib/command/types: fix wrong param name for values

--- lib/command/types.ts
+++ lib/command/types.ts
@@ -2,15 +2,15 @@ import * as Option from '../option/types'
 import * as Program from '../program/types'
 
 export interface Command {
-  description(description?: string): string | Command | Program.Program
+  description(value?: string): string | Command | Program.Program
 
   action(callback: CommandAction): Command | Program.Program
   option(slug: string, description: string, filter?: Option.OptionFilter): Command | Program.Program
-  value(helpName: string, description: string): Command | Program.Program
+  value(name: string, description: string): Command | Program.Program
 }
 
 export type CommandAction = (args: CommandActionArgs) => void | Promise<void>
 export type CommandActionArgs = {
   options: { [name: string]: boolean | number | string }
-  values: Array<number | string>
+  values: { [name: string]: boolean | number | string }
 }
