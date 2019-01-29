e184293bfbed827465eed57c52bdd03516177177
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-29 10:43:21 +0100

    lib/command/types: add action() method types

--- lib/command/types.ts
+++ lib/command/types.ts
@@ -4,5 +4,12 @@ import * as Program from '../program/types'
 export interface Command {
   description(description?: string): string | Command | Program.Program
 
+  action(callback: CommandAction): Command | Program.Program
   option(slug: string, description: string, filter?: Option.OptionFilter): Command | Program.Program
+}
+
+export type CommandAction = (args: CommandActionArgs) => void | Promise<void>
+export type CommandActionArgs = {
+  options: { [name: string]: boolean | number | string }
+  values: Array<number | string>
 }
