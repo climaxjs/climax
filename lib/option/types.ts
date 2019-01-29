35d789b4c9dfcd451271fdc8b93df8e5f9180964
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-29 12:27:26 +0100

    lib/option/types: shorten OptionFilter to Filter

--- lib/option/types.ts
+++ lib/option/types.ts
@@ -1,10 +1,10 @@
 import * as Filter from '../filter/types'
 
-export type OptionFilter = Filter.IsUsable | OptionFilterCustom
-export type OptionFilterCustom = (value?: string) => boolean | number | string
+export type Filter = Filter.IsUsable | FilterCustom
+export type FilterCustom = (value?: string) => boolean | number | string
 
 export interface Option {
   description: string
   slug: string
-  filter?: OptionFilter
+  filter?: Filter
 }
