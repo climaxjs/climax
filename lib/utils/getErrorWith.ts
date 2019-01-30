5c12a1cbabf5c4d6241c269d7ca13779f60cd496
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-30 03:49:15 +0100

    lib/utils/getErrorWith: adapt to RorreError

--- lib/utils/getErrorWith.ts
+++ lib/utils/getErrorWith.ts
@@ -2,8 +2,12 @@
  * Generate an error with custom variables and format specifiers.
  */
 
+import { RorreError } from 'rorre'
 import { sprintf } from 'sprintf-js'
 
-export default function getErrorWith(_message: string, ..._args: any[]): Error {
-  return new Error(sprintf.apply(null, arguments))
+export default function getErrorWith(
+  error: RorreError<string, string>,
+  ...args: any[]
+): Error {
+  return new Error(sprintf.apply(null, [error.message, ...args]))
 }
