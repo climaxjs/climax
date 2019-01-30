87412bd7437f5c1b9759ecd872ea9087ee43032c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-30 04:44:15 +0100

    lib/utils: update

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,9 +1,11 @@
+import logT from './logT'
 import parseArgs from './parseArgs'
 import throwWith from './throwWith'
 import validateOptionSlug from './validateOptionSlug'
 import validateSemVer from './validateSemVer'
 
 export {
+  logT,
   parseArgs,
   throwWith,
   validateOptionSlug,
