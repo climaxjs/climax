8b73ce88338bc25f4e91b9981e9eb25af254c876
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-05 09:26:23 +0100

    scripts/ci/test_coverage: switch to npm

--- scripts/ci/test_coverage.sh
+++ scripts/ci/test_coverage.sh
@@ -3,5 +3,5 @@
 # https://www.gnu.org/software/bash/manual/bashref.html#The-Set-Builtin
 set -e
 
-yarn run test:unit --coverage
+npm run test:unit --coverage
 ./node_modules/.bin/coveralls < ./coverage/lcov.info
