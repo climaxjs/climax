e449befe7b5984e73e6ff42f302e5151a7f760dc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-09 12:13:33 +0100

    lib/utils/parseArgs: simplify

--- lib/utils/parseArgs.spec.ts
+++ lib/utils/parseArgs.spec.ts
@@ -1,28 +1,36 @@
 import parseArgs from './parseArgs'
 
 const MATCHES = [
-  [ [],                                             [null, {}, []]],
-  [ ['--bar'],                                      [null, { bar: true }, []]],
-  [ ['--bar=barValue'],                             [null, { bar: 'barValue' }, []]],
-  [ ['--bar=barValue', '--bar2'],                   [null, { bar: 'barValue', bar2: true }, []]],
-  [ ['--bar=barValue', '--bar2=bar2Value'],         [null, { bar: 'barValue', bar2: 'bar2Value' }, []]],
-  [ ['--bar', '--bar2'],                            [null, { bar: true, bar2: true }, []]],
-  [ ['--bar', '--bar2=bar2Value'],                  [null, { bar: true, bar2: 'bar2Value' }, []]],
+  [ [],       [],                                             [null, {}, []]],
+  [ [],       ['--bar'],                                      [null, { bar: true }, []]],
+  [ [],       ['--bar=barValue'],                             [null, { bar: 'barValue' }, []]],
+  [ [],       ['--bar=barValue', '--bar2'],                   [null, { bar: 'barValue', bar2: true }, []]],
+  [ [],       ['--bar=barValue', '--bar2=bar2Value'],         [null, { bar: 'barValue', bar2: 'bar2Value' }, []]],
+  [ [],       ['--bar', '--bar2'],                            [null, { bar: true, bar2: true }, []]],
+  [ [],       ['--bar', '--bar2=bar2Value'],                  [null, { bar: true, bar2: 'bar2Value' }, []]],
+  [ [],       ['--bar', 'barValue'],                          [null, { bar: 'barValue' }, []]],
+  [ [],       ['--bar', 'barValue', '--bar2'],                [null, { bar: 'barValue', bar2: true }, []]],
+  [ [],       ['--bar', 'barValue', '--bar2', 'bar2Value'],   [null, { bar: 'barValue', bar2: 'bar2Value' }, []]],
+  [ [],       ['--bar', '--bar2'],                            [null, { bar: true, bar2: true }, []]],
+  [ [],       ['--bar', '--bar2', 'bar2Value'],               [null, { bar: true, bar2: 'bar2Value' }, []]],
 
-  [ ['foo'],                                        ['foo', {}, []]],
-  [ ['foo', '--bar'],                               ['foo', { bar: true }, []]],
-  [ ['foo', '--bar=barValue'],                      ['foo', { bar: 'barValue' }, []]],
-  [ ['foo', '--bar=barValue', '--bar2'],            ['foo', { bar: 'barValue', bar2: true }, []]],
-  [ ['foo', '--bar=barValue', '--bar2=bar2Value'],  ['foo', { bar: 'barValue', bar2: 'bar2Value' }, []]],
-  [ ['foo', '--bar', '--bar2'],                     ['foo', { bar: true, bar2: true }, []]],
-  [ ['foo', '--bar', '--bar2=bar2Value'],           ['foo', { bar: true, bar2: 'bar2Value' }, []]],
+  [ [],       ['foo'],                                        [null, {}, ['foo']]],
+  [ [],       ['foo', '--bar'],                               [null, { bar: true }, ['foo']]],
+  [ [],       ['foo', '--bar=barValue'],                      [null, { bar: 'barValue' }, ['foo']]],
+  [ [],       ['foo', '--bar=barValue', '--bar2'],            [null, { bar: 'barValue', bar2: true }, ['foo']]],
+  [ [],       ['foo', '--bar=barValue', '--bar2=bar2Value'],  [null, { bar: 'barValue', bar2: 'bar2Value' }, ['foo']]],
+  [ [],       ['foo', '--bar', '--bar2'],                     [null, { bar: true, bar2: true }, ['foo']]],
+  [ [],       ['foo', '--bar', '--bar2=bar2Value'],           [null, { bar: true, bar2: 'bar2Value' }, ['foo']]],
 
-  [ ['foo', 'val'],                                 ['foo', {}, ['val']]],
-  [ ['foo', 'val', 'val2'],                         ['foo', {}, ['val', 'val2']]],
-  [['foo', '--bar', 'val'],                         ['foo', { bar: true }, ['val']]],
-  [['foo', '--bar=barValue', 'val'],                ['foo', { bar: 'barValue' }, ['val']]],
-  [['foo', 'val', '--bar'],                         ['foo', { bar: true }, ['val']]],
-  [['foo', 'val', '--bar=barValue'],                ['foo', { bar: 'barValue' }, ['val']]],
+  [ ['foo'],  ['foo', 'val'],                                 ['foo', {}, ['val']]],
+  [ ['foo'],  ['foo', 'val', 'val2'],                         ['foo', {}, ['val', 'val2']]],
+  // Misleading case: is "val" the value of --bar option or is --bar a boolean option followed by a lone "val" value?
+  [ ['foo'],  ['foo', '--bar', 'val'],                        ['foo', { bar: 'val' }, []]],
+  [ ['foo'],  ['foo', '--bar=barValue', 'val'],               ['foo', { bar: 'barValue' }, ['val']]],
+  [ ['foo'],  ['foo', 'val', '--bar'],                        ['foo', { bar: true }, ['val']]],
+  [ ['foo'],  ['foo', 'val', '--bar=barValue'],               ['foo', { bar: 'barValue' }, ['val']]],
 ]
 
-MATCHES.map(match => test(`should return the expected result`, () => expect(parseArgs(match[0] as any)).toEqual(match[1])))
+MATCHES.map(match =>
+  test(`should return the expected result`, () =>
+    expect(parseArgs(match[0] as string[], match[1] as string[])).toEqual(match[2])))
