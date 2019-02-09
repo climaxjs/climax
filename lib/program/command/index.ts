2dc7d0d3b78dc025c8f0e36020b0ab26e38519f7
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-09 18:45:39 +0100

    lib/program/command: add options and values name matching"

--- lib/program/command/index.ts
+++ lib/program/command/index.ts
@@ -1,9 +1,13 @@
+import log from '@inspired-beings/log'
+import * as R from 'ramda'
+
 import errors from '../../errors'
 import { throwWith } from '../../utils'
 import Option from '../option'
 import Value from '../value'
 
 import * as T from './types'
+import * as U from '../../types'
 import * as OptionT from '../option/types'
 import * as ValueT from '../value/types'
 const { error: E } = errors
@@ -108,7 +112,64 @@ export default class Command implements T.Command {
   /**
    * Run the action callback passing it the command options and values.
    */
-  public run(values: {}, options: {}): void {
-    this._action({ options, values })
+  public run(rawOptions: U.BNSObject, rawValues: U.BNS[]): void {
+    const preOptions = this.matchOptionsName(rawOptions)
+    const preValues = this.matchValuesName(rawValues)
+
+    // Now, we may have some values following boolean options that have been
+    // parsed as being those options values. We thus need to clean them by
+    // switching back their values to the lone values list.
+    // TODO Add the pre-options cleaning.
+
+    this._action({ options: preOptions, values: preValues })
+  }
+
+  /**
+   * Translate short slug names and remove undeclared options.
+   */
+  private matchOptionsName(rawOptions: U.BNSObject): U.BNSObject {
+    return R.compose(
+      R.reduce(
+        (acc, [slug, value]) => {
+          if (slug.length > 1) {
+            if (R.contains({ slug }, this._options)) {
+              return R.assoc(slug, value, acc)
+            }
+
+            log.warn(`Unknow option %s. Please run --help.`, `--${slug}`)
+
+            return { ...acc }
+          }
+
+          const found = R.find(R.propEq('slugLetter', slug), this._options)
+          if (found !== undefined) {
+            return R.assoc(found.slug, value, acc)
+          }
+
+          log.warn(`Unknow option %s. Please run --help.`, `-${slug}`)
+
+          return { ...acc }
+        },
+        {}),
+      R.toPairs,
+    )(rawOptions)
+  }
+
+  /**
+   * Match values with their declared names and remove undeclared values.
+   */
+  private matchValuesName(rawValues: U.BNS[]): U.BNSObject {
+    return rawValues.reduce(
+      (acc, value, index) => {
+        if (this._values[index]) {
+          return R.assoc(this._values[index].name, value, acc)
+        }
+
+        log.warn(`Too many values ("%s"). Please run --help.`, String(value))
+
+        return { ...acc }
+      },
+      {},
+    )
   }
 }
