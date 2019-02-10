4c971fff43f9b75a1dd602d70a817ef578a09f2d
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-10 13:06:53 +0100

    lib/program/option/types: lint

--- lib/program/option/types.ts
+++ lib/program/option/types.ts
@@ -1,7 +1,8 @@
+import { BNS } from '../../types'
 import * as Filter from '../filter/types'
 
-export type Filter = Filter.IsUsable | FilterCustom
-export type FilterCustom = (value?: string) => boolean | number | string
+export type Filter = Filter.IsFinal | FilterCustom
+export type FilterCustom = (value: BNS) => BNS | null
 
 export interface Option {
   readonly description: string
