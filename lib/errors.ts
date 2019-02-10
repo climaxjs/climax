b0d92afd1da2686b70d84ff8568682ae7613cdb0
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-10 20:58:53 +0100

    lib: merge Value and Option

--- lib/errors.ts
+++ lib/errors.ts
@@ -10,15 +10,7 @@ export default rorre.declare({
   ERR_CMD_DESC_V_TYP: `The command (or program) description is mandatory and must be a string.`,
   ERR_CMD_PROC_X_FLT_C: `The provided custom filter threw an error.`,
 
-  ERR_FLT_TYPE_I_UND: `The filter type is undefined. Did you attempt to call #isBoolean() from an unset filter?`,
-
-  ERR_OPT_DESC_V_LEN: `[Option: "%s"] The option description can't be empty.`,
-  ERR_OPT_DESC_V_TYP: `[Option: "%s"] The option description is mandatory and must be a string.`,
-  ERR_OPT_FILT_V_TYP: `[Option: "%s"] The option filter is not processable as is. Specify, at least, both the obligation and type.`,
-  ERR_OPT_FILT_V_TYP_C: `[Option: "%s"] If you want to use a custom option filter, this filter must be a function.`,
-  ERR_OPT_SLUG_V_FMT: `[Option: "%s"] The option slug format is wrong.`,
-  ERR_OPT_SLUG_V_LEN: `The option slug can't be empty.`,
-  ERR_OPT_SLUG_V_TYP: `The option slug is mandatory and must be a string.`,
+  ERR_OPT_SLUG_V_FMT: `The option slug format is wrong.`,
 
   ERR_PRG_ARGS_V_LEN: `Something went wrong during the arguments validation (length < 2).`,
   ERR_PRG_ARGS_V_TYP: `Something went wrong during the arguments validation (not an array).`,
@@ -30,8 +22,10 @@ export default rorre.declare({
   ERR_PRG_VERS_V_TYP: `The program version must be a string.`,
   ERR_PRG_VERS_V_NOV: `The version you set can't start with a "v" or a "V". If you want to declare "vX.Y.Z", just declare "X.Y.Z".`,
 
-  ERR_VAL_DESC_V_LEN: `[Value: "%s"] The value description can't be empty.`,
-  ERR_VAL_DESC_V_TYP: `[Value: "%s"] The value description is mandatory and must be a string.`,
-  ERR_VAL_NAME_V_LEN: `The value name can't be empty.`,
-  ERR_VAL_NAME_V_TYP: `The value name is mandatory and must be a string.`,
+  ERR_VAL_DESC_V_LEN: `The option (or value) description can't be empty.`,
+  ERR_VAL_DESC_V_TYP: `The option (or value) description is mandatory and must be a string.`,
+  ERR_VAL_FILT_V_TYP: `The option (or value) filter is not processable as is. Specify, at least, both the obligation and type.`,
+  ERR_VAL_FILT_V_TYP_C: `If you want to use a custom option (or value) filter, this filter must be a function.`,
+  ERR_VAL_NAME_V_LEN: `The option slug (or value name) can't be empty.`,
+  ERR_VAL_NAME_V_TYP: `The option slug (or value name) is mandatory and must be a string.`,
 })
--- lib/program/option/index.ts
+++ lib/program/option/index.ts
@@ -3,44 +3,27 @@
  */
 
 import errors from '../../errors'
-import { throwF, validateOptionSlug } from '../../utils'
+import { validateOptionSlug, throwWith } from '../../utils'
+import Value from '../value'
 
 import * as T from './types'
+import * as ValueT from '../value/types'
 const { error: E } = errors
 
-export default class Option implements T.Option {
+export default class Option extends Value implements T.Option {
   public readonly slug: string
   public readonly slugLetter: string | null
 
   constructor(
     slug: string,
     public readonly description: string,
-    public readonly filter?: T.Filter,
+    public readonly filter?: ValueT.Filter,
   ) {
+    super(slug, description, filter)
+
     switch (true) {
-      case typeof slug !== 'string':
-        throw E.ERR_OPT_SLUG_V_TYP
-
-      case slug.length === 0:
-        throw E.ERR_OPT_SLUG_V_LEN
-
       case !validateOptionSlug(slug):
-        throwF(E.ERR_OPT_SLUG_V_FMT, slug)
-
-      case typeof description !== 'string':
-        throwF(E.ERR_OPT_DESC_V_TYP, slug)
-
-      case description.length === 0:
-        throwF(E.ERR_OPT_DESC_V_LEN, slug)
-
-      case filter !== undefined:
-        switch (true) {
-          case this.isUnusableInternalFilter(filter):
-            throwF(E.ERR_OPT_FILT_V_TYP, slug)
-
-          case !this.isInternalFilter(filter) && typeof filter !== 'function':
-            throwF(E.ERR_OPT_FILT_V_TYP_C, slug)
-        }
+        throwWith(E.ERR_OPT_SLUG_V_FMT, this._e)
     }
 
     const slugs = slug
@@ -52,30 +35,5 @@ export default class Option implements T.Option {
 
     this.slug = slugs.length === 2 ? slugs[1] : slugs[0]
     this.slugLetter = slugs.length === 2 ? slugs[0] : null
-  }
-
-  /**
-   * Is this filter the instance of a usable internal filter?
-   */
-  private isInternalFilter(filter: any): boolean {
-    return typeof filter === 'object' && (
-      [
-        'Is',
-        'IsObligation',
-        'IsType',
-        'IsBoolean',
-        'IsNumber',
-        'IsString',
-      ].includes(filter.constructor.name)
-    )
-  }
-
-  /**
-   * Is this filter the instance of an unusable (= not final) internal filter?
-   */
-  private isUnusableInternalFilter(filter: any): boolean {
-    return typeof filter === 'object' && (
-      ['Is', 'IsObligation', 'IsType'].includes(filter.constructor.name)
-    )
   }
 }
