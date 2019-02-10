a7d0537d1b43f436c99df166b62926224b035fd1
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-10 22:10:35 +0100

    lib/program: add info() method

--- lib/program/types.ts
+++ lib/program/types.ts
@@ -4,7 +4,18 @@ export type ProgramCommands = {
   [slug: string]: Command.Command
 }
 
+export type NpmInfo = {
+  name: string
+  description: string
+  version: string
+  bin: {
+    [key: string]: string
+  }
+  [key: string]: any
+}
+
 export interface Program extends Command.Command {
+  info(npmInfo: NpmInfo): this
   name(name: string): this
   version(version: string): this
 
