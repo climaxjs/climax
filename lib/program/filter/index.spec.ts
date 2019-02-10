55d4624fbf9fe52b6fe0fd7650455f286a201bfa
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-10 14:27:55 +0100

    lib/program/filter/index.spec: update

--- lib/program/filter/index.spec.ts
+++ lib/program/filter/index.spec.ts
@@ -9,46 +9,46 @@ describe(`is`, () => {
     describe(`#boolean`, () => {
       const filter = is.aMandatory.boolean
 
-      it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+      it(`should return a class instance of IsBoolean`, () => expect(filter.constructor.name).toBe('IsBoolean'))
     })
 
     describe(`#float`, () => {
       const filter = is.aMandatory.float
 
-      it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+      it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
     })
 
     describe(`#integer`, () => {
       const filter = is.aMandatory.integer
 
-      it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+      it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-      it(`should invalidate a float`, () => expect(filter.validate(1.2)).toBe(false))
-      it(`should validate an integer`, () => expect(filter.validate(1)).toBe(true))
+      it(`should invalidate a float`, () => expect(() => filter.validate(1.2)).toThrow())
+      it(`should validate an integer`, () => expect(() => filter.validate(1)).not.toThrow())
 
       describe(`#between()`, () => {
         describe(`with -1, 1`, () => {
           const filter = is.aMandatory.integer.between(-1, 1)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should invalidate -2`, () => expect(filter.validate(-2)).toBe(false))
-          it(`should invalidate -1`, () => expect(filter.validate(-1)).toBe(false))
-          it(`should validate 0`, () => expect(filter.validate(0)).toBe(true))
-          it(`should invalidate 1`, () => expect(filter.validate(1)).toBe(false))
-          it(`should invalidate 2`, () => expect(filter.validate(2)).toBe(false))
+          it(`should invalidate -2`, () => expect(() => filter.validate(-2)).toThrow())
+          it(`should invalidate -1`, () => expect(() => filter.validate(-1)).toThrow())
+          it(`should validate 0`, () => expect(() => filter.validate(0)).not.toThrow())
+          it(`should invalidate 1`, () => expect(() => filter.validate(1)).toThrow())
+          it(`should invalidate 2`, () => expect(() => filter.validate(2)).toThrow())
         })
 
         describe(`with -1, 1, true`, () => {
           const filter = is.aMandatory.integer.between(-1, 1, true)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should invalidate -2`, () => expect(filter.validate(-2)).toBe(false))
-          it(`should validate -1`, () => expect(filter.validate(-1)).toBe(true))
-          it(`should validate 0`, () => expect(filter.validate(0)).toBe(true))
-          it(`should validate 1`, () => expect(filter.validate(1)).toBe(true))
-          it(`should invalidate 2`, () => expect(filter.validate(2)).toBe(false))
+          it(`should invalidate -2`, () => expect(() => filter.validate(-2)).toThrow())
+          it(`should validate -1`, () => expect(() => filter.validate(-1)).not.toThrow())
+          it(`should validate 0`, () => expect(() => filter.validate(0)).not.toThrow())
+          it(`should validate 1`, () => expect(() => filter.validate(1)).not.toThrow())
+          it(`should invalidate 2`, () => expect(() => filter.validate(2)).toThrow())
         })
       })
 
@@ -56,21 +56,21 @@ describe(`is`, () => {
         describe(`with -1`, () => {
           const filter = is.aMandatory.integer.greaterThan(-1)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should invalidate -2`, () => expect(filter.validate(-2)).toBe(false))
-          it(`should invalidate -1`, () => expect(filter.validate(-1)).toBe(false))
-          it(`should validate 0`, () => expect(filter.validate(0)).toBe(true))
+          it(`should invalidate -2`, () => expect(() => filter.validate(-2)).toThrow())
+          it(`should invalidate -1`, () => expect(() => filter.validate(-1)).toThrow())
+          it(`should validate 0`, () => expect(() => filter.validate(0)).not.toThrow())
         })
 
         describe(`with -1, true`, () => {
           const filter = is.aMandatory.integer.greaterThan(-1, true)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should invalidate -2`, () => expect(filter.validate(-2)).toBe(false))
-          it(`should validate -1`, () => expect(filter.validate(-1)).toBe(true))
-          it(`should validate 0`, () => expect(filter.validate(0)).toBe(true))
+          it(`should invalidate -2`, () => expect(() => filter.validate(-2)).toThrow())
+          it(`should validate -1`, () => expect(() => filter.validate(-1)).not.toThrow())
+          it(`should validate 0`, () => expect(() => filter.validate(0)).not.toThrow())
         })
       })
 
@@ -78,21 +78,21 @@ describe(`is`, () => {
         describe(`with -1`, () => {
           const filter = is.aMandatory.integer.lessThan(-1)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should validate -2`, () => expect(filter.validate(-2)).toBe(true))
-          it(`should invalidate -1`, () => expect(filter.validate(-1)).toBe(false))
-          it(`should invalidate 0`, () => expect(filter.validate(0)).toBe(false))
+          it(`should validate -2`, () => expect(() => filter.validate(-2)).not.toThrow())
+          it(`should invalidate -1`, () => expect(() => filter.validate(-1)).toThrow())
+          it(`should invalidate 0`, () => expect(() => filter.validate(0)).toThrow())
         })
 
         describe(`with -1, true`, () => {
-          const filter = is.aMandatory.integer.lessThan(-1, true)
+          const filter = is.aMandatory.integer.lessThan(-1, true);
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsNumber`, () => expect(filter.constructor.name).toBe('IsNumber'))
 
-          it(`should validate -2`, () => expect(filter.validate(-2)).toBe(true))
-          it(`should validate -1`, () => expect(filter.validate(-1)).toBe(true))
-          it(`should invalidate 0`, () => expect(filter.validate(0)).toBe(false))
+          it(`should validate -2`, () => expect(() => filter.validate(-2)).not.toThrow())
+          it(`should validate -1`, () => expect(() => filter.validate(-1)).not.toThrow())
+          it(`should invalidate 0`, () => expect(() => filter.validate(0)).toThrow())
         })
       })
     })
@@ -100,25 +100,25 @@ describe(`is`, () => {
     describe(`#string`, () => {
       const filter = is.aMandatory.string
 
-      it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+      it(`should return a class instance of IsString`, () => expect(filter.constructor.name).toBe('IsString'))
 
       describe(`#longerThan()`, () => {
         describe(`with 0`, () => {
           const filter = is.aMandatory.string.longerThan(0)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsString`, () => expect(filter.constructor.name).toBe('IsString'))
 
-          it(`should invalidate ""`, () => expect(filter.validate("")).toBe(false))
-          it(`should validate "a"`, () => expect(filter.validate("a")).toBe(true))
+          it(`should invalidate ""`, () => expect(() => filter.validate("")).toThrow())
+          it(`should validate "a"`, () => expect(() => filter.validate("a")).not.toThrow())
         })
 
         describe(`with 0, true`, () => {
           const filter = is.aMandatory.string.longerThan(0, true)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsString`, () => expect(filter.constructor.name).toBe('IsString'))
 
-          it(`should validate ""`, () => expect(filter.validate("")).toBe(true))
-          it(`should validate "a"`, () => expect(filter.validate("a")).toBe(true))
+          it(`should validate ""`, () => expect(() => filter.validate("")).not.toThrow())
+          it(`should validate "a"`, () => expect(() => filter.validate("a")).not.toThrow())
         })
       })
 
@@ -126,19 +126,19 @@ describe(`is`, () => {
         describe(`with 2`, () => {
           const filter = is.aMandatory.string.shorterThan(2)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsString`, () => expect(filter.constructor.name).toBe('IsString'))
 
-          it(`should validate "a"`, () => expect(filter.validate("a")).toBe(true))
-          it(`should invalidate "ab"`, () => expect(filter.validate("ab")).toBe(false))
+          it(`should validate "a"`, () => expect(() => filter.validate("a")).not.toThrow())
+          it(`should invalidate "ab"`, () => expect(() => filter.validate("ab")).toThrow())
         })
 
         describe(`with 2, true`, () => {
           const filter = is.aMandatory.string.shorterThan(2, true)
 
-          it(`should return a class instance of IsFinal`, () => expect(filter.constructor.name).toBe('IsFinal'))
+          it(`should return a class instance of IsString`, () => expect(filter.constructor.name).toBe('IsString'))
 
-          it(`should validate "a"`, () => expect(filter.validate("a")).toBe(true))
-          it(`should validate "ab"`, () => expect(filter.validate("ab")).toBe(true))
+          it(`should validate "a"`, () => expect(() => filter.validate("a")).not.toThrow())
+          it(`should validate "ab"`, () => expect(() => filter.validate("ab")).not.toThrow())
         })
       })
     })
