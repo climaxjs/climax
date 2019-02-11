82354e34dad726cdcdb8d0012346337648baa778
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-11 07:30:24 +0100

    lib/utils: remove throwF

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,6 +1,5 @@
 import logT from './logT'
 import parseArgs from './parseArgs'
-import throwF from './throwF'
 import throwWith from './throwWith'
 import validateOptionSlug from './validateOptionSlug'
 import validateSemVer from './validateSemVer'
@@ -8,7 +7,6 @@ import validateSemVer from './validateSemVer'
 export {
   logT,
   parseArgs,
-  throwF,
   throwWith,
   validateOptionSlug,
   validateSemVer,
