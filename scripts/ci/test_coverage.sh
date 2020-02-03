50ea9a8ea59cef604df8ac0645f691ac82ac1744
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-02-03 01:54:02 +0100

    ci(travis): simplify unit tests

--- scripts/ci/test_coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# https://www.gnu.org/software/bash/manual/bashref.html#The-Set-Builtin
-set -e
-
-npm run test:unit -- --coverage
-./node_modules/.bin/coveralls < ./coverage/lcov.info
